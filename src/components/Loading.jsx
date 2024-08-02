9ee923db5ace0e85e8376252f5f7747bc2b1083f
Adhamjon Zoyitov <adhamjonzokhidov@gmail.com>
2024-08-02 21:19:13 +0500

    loading add all done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="loading">
-      <h1>Loading...</h1>
+        <div class='flex space-x-2 justify-center items-center bg-transparent h-screen'>
+          <span class='sr-only'>Loading...</span>
+          <div class='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+          <div class='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+          <div class='h-8 w-8 bg-white rounded-full animate-bounce'></div>
+        </div>
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,8 @@ const Nav = () => {
     <div className="w-[100%] h-[62px] bg-[rgba(2,6,23,1)] fixed top-0 left-0 flex items-center justify-center z-50">
       <div className="w-[80%] h-[35px] flex justify-between items-center">
         <ul className="flex items-center text-2xl w-[10%]">
-          <li className="text-[rgba(234,179,8,1)]">Z</li>
-          <li className="text-white">oyitov.</li>
+          <li className="text-[rgba(234,179,8,1)]"><a href="#hero">Z</a></li>
+          <li className="text-white"><a href="#hero">oyitov.</a></li>
         </ul>
         <ul className="flex items-center justify-between w-[55%] text-[16px] text-base h-[100%] font-regular">
           <li className="text-gray-500 hover:text-white transition duration-1500 cursor-pointer">
